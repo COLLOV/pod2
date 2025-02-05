1ed79ad7a7c319d96da82096a40c10af7f7f8f14
COLLOV <collov.j@gmail.com>
2025-02-06 00:01:13 +0100

    dds

--- test_local.py
+++ test_local.py
@@ -1,9 +1,15 @@
 import requests
-from dotenv import load_dotenv
 import os
 
+def load_env():
+    with open('.env') as f:
+        for line in f:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                os.environ[key] = value
+
 # Charger les variables d'environnement depuis .env
-load_dotenv()
+load_env()
 
 # Récupérer le POD_ID depuis les variables d'environnement
 POD_ID = os.getenv('POD_ID')
